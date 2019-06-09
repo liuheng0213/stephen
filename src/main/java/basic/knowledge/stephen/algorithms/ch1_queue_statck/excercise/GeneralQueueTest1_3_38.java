21cf1fed5633153af37adadb41c4a3bd48d2e169
Cooper <449836902@qq.com>
2019-06-09 15:22:31 +0800

    ass

---
+++ src/main/java/basic/knowledge/stephen/algorithms/ch1_queue_statck/excercise/GeneralQueueTest1_3_38.java
@@ -0,0 +1,57 @@
+package basic.knowledge.stephen.algorithms.ch1_queue_statck.excercise;
+
+import basic.knowledge.stephen.algorithms.ch1_queue_statck.queue.GeneralizedQueueByArray;
+import basic.knowledge.stephen.algorithms.ch1_queue_statck.queue.GeneralizedQueueByLink;
+import basic.knowledge.stephen.algorithms.ch1_queue_statck.queue.MyQueue;
+import basic.knowledge.stephen.algorithms.ch1_queue_statck.queue.User;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+public class GeneralQueueTest1_3_38 {
+    @Test
+    public void test1(){
+        GeneralizedQueueByArray<Integer> myqueue = new GeneralizedQueueByArray<>();
+        myqueue.enqueue(1);
+        myqueue.enqueue(3);
+        myqueue.enqueue(5);
+        myqueue.enqueue(3);
+        myqueue.enqueue(9);
+        myqueue.enqueue(3);
+        myqueue.enqueue(10);
+
+        Integer delete = myqueue.delete(3);
+        System.out.println(delete);
+
+        System.out.println("=========================");
+
+        Iterator<Integer> iterator = myqueue.iterator();
+        while(iterator.hasNext()){
+            Integer i = iterator.next();
+            System.out.println(i);
+        }
+    }
+
+    @Test
+    public void test2(){
+        GeneralizedQueueByLink<Integer> myqueue = new GeneralizedQueueByLink<>();
+        myqueue.enqueue(1);
+        myqueue.enqueue(3);
+        myqueue.enqueue(5);
+        myqueue.enqueue(3);
+        myqueue.enqueue(9);
+        myqueue.enqueue(3);
+        myqueue.enqueue(10);
+
+        Integer delete = myqueue.delete(3);
+        System.out.println(delete);
+
+        System.out.println("=========================");
+
+        Iterator<Integer> iterator = myqueue.iterator();
+        while(iterator.hasNext()){
+            Integer i = iterator.next();
+            System.out.println(i);
+        }
+    }
+}
--- src/main/java/basic/knowledge/stephen/algorithms/ch1_queue_statck/queue/GeneralizedQueueByLink.java
+++ src/main/java/basic/knowledge/stephen/algorithms/ch1_queue_statck/queue/GeneralizedQueueByLink.java
@@ -1,7 +1,107 @@
 package basic.knowledge.stephen.algorithms.ch1_queue_statck.queue;
 
+
+import java.util.Iterator;
+
 /**
  * 练习1.3.38
  */
-public class GeneralizedQueueByLink {
+public class GeneralizedQueueByLink<Item> implements Iterable<Item> {
+    private Node first;
+    private Node last;
+    private int N;
+
+
+    private class Node {
+        Item t;
+        Node next;
+        Node previous;
+    }
+
+    public boolean isEmpty() {
+        return first == null && last == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    //放到last后  dequeue从first出
+    public void enqueue(Item t) {
+        if (!isEmpty()) {
+            Node oldLast = last;
+            Node current = new Node();
+            current.t = t;
+            current.next = oldLast;
+            current.previous = null;
+            last = current;
+            oldLast.previous = last;
+        } else {
+            Node current = new Node();
+            current.t = t;
+            current.next = null;
+            current.previous = null;
+            last = current;
+            first = current;
+        }
+        N++;
+    }
+
+    public Item dequeue() {
+        if (!isEmpty()) {
+            Node oldFirst = first;
+            oldFirst.previous.next = null;
+            first = oldFirst.previous;
+            N--;
+            return oldFirst.t;
+        }
+        return null;
+    }
+
+    /**
+     * 练习1.3.38
+     *
+     * @param k
+     * @return
+     */
+    public Item delete(Item k) {
+        Node first = this.first;
+        for (int i = 1; i <= this.N; i++) {
+            if (first.t == k) {
+                first.previous.next = first.next;
+                first.next.previous = first.previous;
+                this.N--;
+                return first.t;
+            } else {
+                first = first.previous;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new GeneralizedQueueByLinkIterator();
+    }
+
+    private class GeneralizedQueueByLinkIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item t = current.t;
+            current = current.previous;
+            return t;
+        }
+
+        @Override
+        public void remove() {
+            //do nothing
+        }
+    }
 }

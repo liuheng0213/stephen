a2f47da12bfe8cd243c4777d5bfaa787abd81584
Cooper <449836902@qq.com>
2019-06-28 02:20:41 +0800

    dd

--- src/main/java/basic/knowledge/stephen/algorithm_4_Edition/mock/MockData.java
+++ src/main/java/basic/knowledge/stephen/algorithm_4_Edition/mock/MockData.java
@@ -3,7 +3,9 @@ package basic.knowledge.stephen.algorithm_4_Edition.mock;
 import basic.knowledge.stephen.algorithm_4_Edition.ch1.entity.User;
 
 public class MockData {
+    public static final Comparable[] DOUBLE_FOR_SORT_MOCK = new Double[]{0.0,1.0,2.0,1.2,1.3,1.0,8.0,4.5,3.6,7.2};
     public static final Comparable[] FOR_SORT_MOCK = getUser();
+
 
     private static Comparable[] getUser() {
 
@@ -11,6 +13,4 @@ public class MockData {
         ,new User(5),new User(16),new User(71),new User(18),new User(9), new User(0),
                 new User(13)};
     }
-
-    public static final Comparable[] DOUBLE_FOR_SORT_MOCK = new Double[]{0.0,1.0,2.0,1.2,1.3,1.0,8.0,4.5,3.6,7.2};
 }

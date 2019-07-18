733ce1a9ee86ac0301739e0bbb7ac12f45fa8f8c
Cooper <449836902@qq.com>
2019-07-19 01:25:01 +0800

    dd

--- src/main/java/basic/knowledge/stephen/algorithm_4_Edition/ch2/sort_04_heap/_05E2_4_24.java
+++ src/main/java/basic/knowledge/stephen/algorithm_4_Edition/ch2/sort_04_heap/_05E2_4_24.java
@@ -3,13 +3,28 @@ package basic.knowledge.stephen.algorithm_4_Edition.ch2.sort_04_heap;
 import basic.knowledge.stephen.algorithm_4_Edition.util.SortUtil;
 
 public class _05E2_4_24<Item extends Comparable<Item>> {
-    private class TreeNode<Item>{
+    public static void main(String[] args) {
+        _05E2_4_24 pq = new _05E2_4_24();
+        pq.insert(11);
+        pq.insert(11);
+        pq.insert(2);
+        pq.insert(10);
+        pq.insert(11);
+        pq.insert(11);
+        pq.insert(110);
+
+        System.out.println(pq.delMax());
+        System.out.println(pq.size());
+    }
+
+    private class TreeNode<Item> {
         Item item;
-        TreeNode Prev;
-        TreeNode Left;
-        TreeNode Right;
+        TreeNode prev;
+        TreeNode left;
+        TreeNode right;
 
     }
+
     /// <summary>
     /// 二叉堆的根结点。
     /// </summary>
@@ -26,26 +41,23 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
     /// <summary>
     /// 建立一个链式结构的最大堆。
     /// </summary>
-    public _05E2_4_24() { }
+    public _05E2_4_24() {
+    }
 
     /// <summary>
     /// 删除并返回最大值。
     /// </summary>
     /// <returns>最大值。</returns>
-    public Item DelMax()
-    {
+    public Item delMax() {
         Item result = this.root.item;
-        Exch(this.root, this.last);
+        exch(this.root, this.last);
 
-        if (this.nodesCount == 2)
-        {
-            this.root.Left = null;
+        if (this.nodesCount == 2) {
+            this.root.left = null;
             this.last = this.root;
             this.nodesCount--;
             return result;
-        }
-        else if (this.nodesCount == 1)
-        {
+        } else if (this.nodesCount == 1) {
             this.last = null;
             this.root = null;
             this.nodesCount--;
@@ -54,42 +66,38 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
 
         // 获得前一个结点。
         TreeNode<Item> newLast = this.last;
-        if (newLast == this.last.Prev.Right)
-            newLast = this.last.Prev.Left;
-        else
-        {
+        if (newLast == this.last.prev.right)
+            newLast = this.last.prev.left;
+        else {
             // 找到上一棵子树。
-            while (newLast != this.root)
-            {
-                if (newLast != newLast.Prev.Left)
+            while (newLast != this.root) {
+                if (newLast != newLast.prev.left)
                     break;
-                newLast = newLast.Prev;
+                newLast = newLast.prev;
             }
 
             // 已经是满二叉树。
-            if (newLast == this.root)
-            {
+            if (newLast == this.root) {
                 // 一路向右，回到上一层。
-                while (newLast.Right != null)
-                    newLast = newLast.Right;
+                while (newLast.right != null)
+                    newLast = newLast.right;
             }
             // 不是满二叉树。
-            else
-            {
+            else {
                 // 向左子树移动，再一路向右。
-                newLast = newLast.Prev.Left;
-                while (newLast.Right != null)
-                    newLast = newLast.Right;
+                newLast = newLast.prev.left;
+                while (newLast.right != null)
+                    newLast = newLast.right;
             }
         }
 
         // 删除最后一个结点。
-        if (this.last.Prev.Left == this.last)
-            this.last.Prev.Left = null;
+        if (this.last.prev.left == this.last)
+            this.last.prev.left = null;
         else
-            this.last.Prev.Right = null;
+            this.last.prev.right = null;
 
-        Sink(this.root);
+        sink(this.root);
 
         // 指向新的最后一个结点。
         this.last = newLast;
@@ -101,12 +109,10 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
     /// 插入一个新的结点。
     /// </summary>
     /// <param name="v">待插入的结点。</param>
-    public void Insert(Item v)
-    {
+    public void insert(Item v) {
         TreeNode<Item> item = new TreeNode<Item>();
         // 堆为空。
-        if (this.last == null)
-        {
+        if (this.last == null) {
             this.root = item;
             this.last = item;
             this.nodesCount++;
@@ -114,62 +120,55 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
         }
 
         // 堆只有一个结点。
-        if (this.last == this.root)
-        {
-            item.Prev = this.root;
-            this.root.Left = item;
+        if (this.last == this.root) {
+            item.prev = this.root;
+            this.root.left = item;
             this.last = item;
             this.nodesCount++;
-            Swim(item);
+            swim(item);
             return;
         }
 
         // 定位到最后一个节点的父结点。
-        TreeNode<Item> prev = this.last.Prev;
+        TreeNode<Item> prev = this.last.prev;
 
         // 右子节点为空，插入到右子节点。
-        if (prev.Right == null)
-        {
-            item.Prev = prev;
-            prev.Right = item;
-        }
-        else
-        {
+        if (prev.right == null) {
+            item.prev = prev;
+            prev.right = item;
+        } else {
             // 当前子树已满，需要向上回溯。
             // 找到下一个子树（回溯的时候是从左子树回溯上去的）。
-            while (prev != this.root)
-            {
-                if (prev != prev.Prev.Right)
+            while (prev != this.root) {
+                if (prev != prev.prev.right)
                     break;
-                prev = prev.Prev;
+                prev = prev.prev;
             }
 
             // 已经是满二叉树。
-            if (prev == this.root)
-            {
+            if (prev == this.root) {
                 // 一路向左，进入下一层。
-                while (prev.Left != null)
-                    prev = prev.Left;
+                while (prev.left != null)
+                    prev = prev.left;
 
-                item.Prev = prev;
-                prev.Left = item;
+                item.prev = prev;
+                prev.left = item;
             }
             // 不是满二叉树。
-            else
-            {
+            else {
                 // 向右子树移动，再一路向左。
-                prev = prev.Prev.Right;
-                while (prev.Left != null)
-                    prev = prev.Left;
+                prev = prev.prev.right;
+                while (prev.left != null)
+                    prev = prev.left;
 
-                item.Prev = prev;
-                prev.Left = item;
+                item.prev = prev;
+                prev.left = item;
             }
         }
 
         this.last = item;
         this.nodesCount++;
-        Swim(item);
+        swim(item);
         return;
     }
 
@@ -177,7 +176,7 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
     /// 返回二叉堆是否为空。
     /// </summary>
     /// <returns></returns>
-    public boolean IsEmpty(){
+    public boolean isEmpty() {
         return this.root == null;
     }
 
@@ -185,7 +184,7 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
     /// 返回二叉堆中的最大值。
     /// </summary>
     /// <returns></returns>
-    public Item Max() {
+    public Item max() {
         return this.root.item;
     }
 
@@ -193,7 +192,7 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
     /// 返回二叉堆中的元素个数。
     /// </summary>
     /// <returns></returns>
-    public int Size() {
+    public int size() {
         return this.nodesCount;
     }
 
@@ -201,16 +200,12 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
     /// 使结点上浮。
     /// </summary>
     /// <param name="k">需要上浮的结点。</param>
-    private void Swim(TreeNode<Item> k)
-    {
-        while (k.Prev != null)
-        {
-            if (Less(k.Prev, k))
-            {
-                Exch(k.Prev, k);
-                k = k.Prev;
-            }
-            else
+    private void swim(TreeNode<Item> k) {
+        while (k.prev != null) {
+            if (less(k.prev, k)) {
+                exch(k.prev, k);
+                k = k.prev;
+            } else
                 break;
         }
     }
@@ -219,20 +214,18 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
     /// 使结点下沉。
     /// </summary>
     /// <param name="k">需要下沉的结点。</param>
-    private void Sink(TreeNode<Item> k)
-    {
-        while (k.Left != null || k.Right != null)
-        {
+    private void sink(TreeNode<Item> k) {
+        while (k.left != null || k.right != null) {
             TreeNode<Item> toExch = null;
-            if (k.Left != null && k.Right != null)
-                toExch = Less(k.Left, k.Right) ? k.Right : k.Left;
-            else if (k.Left != null)
-                toExch = k.Left;
+            if (k.left != null && k.right != null)
+                toExch = less(k.left, k.right) ? k.right : k.left;
+            else if (k.left != null)
+                toExch = k.left;
             else
-                toExch = k.Right;
+                toExch = k.right;
 
-            if (Less(k, toExch))
-                Exch(k, toExch);
+            if (less(k, toExch))
+                exch(k, toExch);
             else
                 break;
             k = toExch;
@@ -244,8 +237,7 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
     /// </summary>
     /// <param name="a">要交换的第一个结点。</param>
     /// <param name="b">要交换的第二个结点。</param>
-    private void Exch(TreeNode<Item> a, TreeNode<Item> b)
-    {
+    private void exch(TreeNode<Item> a, TreeNode<Item> b) {
         Item temp = a.item;
         a.item = b.item;
         b.item = temp;
@@ -257,8 +249,7 @@ public class _05E2_4_24<Item extends Comparable<Item>> {
     /// <param name="a">判断是否较小的结点。</param>
     /// <param name="b">判断是否较大的结点。</param>
     /// <returns></returns>
-    private boolean Less(TreeNode<Item> a, TreeNode<Item> b){
-
-            return a.item.compareTo(b.item) < 0;
+    private boolean less(TreeNode<Item> a, TreeNode<Item> b) {
+        return a.item.compareTo(b.item) < 0;
     }
 }

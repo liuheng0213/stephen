5bc73424ddbea67064bcc7b1027cc58f2ca3cfe6
Cooper <449836902@qq.com>
2019-08-14 23:44:42 +0800

    dd

--- src/main/java/basic/knowledge/stephen/algorithm_4_Edition/ch2/sort_05_app/_12E2_5_27.java
+++ src/main/java/basic/knowledge/stephen/algorithm_4_Edition/ch2/sort_05_app/_12E2_5_27.java
@@ -1,4 +1,16 @@
 package basic.knowledge.stephen.algorithm_4_Edition.ch2.sort_05_app;
 
+import basic.knowledge.stephen.algorithm_4_Edition.mock.MockData;
+
 public class _12E2_5_27 {
+    public static void main(String[] args) {
+        _12E2_5_27 e2_5_27 = new _12E2_5_27();
+        Integer[] indexArr = e2_5_27.getIndexArr(MockData.SHORT_INTEGER);
+    }
+
+    //并非真正的排列arr,
+    public Integer[] getIndexArr(Comparable[] arr){
+
+        return indexArr;
+    }
 }

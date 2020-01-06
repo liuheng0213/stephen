c33e01df637d30016f696d3e6c46e69bf9f63e16
Cooper <449836902@qq.com>
2020-01-07 02:10:12 +0800

    ff

--- src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/StackAndQueue/_06MaxWindow.java
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/StackAndQueue/_06MaxWindow.java
@@ -6,13 +6,13 @@ import java.util.LinkedList;
 public class _06MaxWindow {
     public static void main(String[] args) {
         _06MaxWindow maxWindow = new _06MaxWindow();
-        int[] arr = new int[]{4,3,5,4,3,3,6,7};
-        int[] res = maxWindow.getMaxWindow(arr,3);
+        int[] arr = new int[]{4, 3, 5, 4, 3, 3, 6, 7};
+        int[] res = maxWindow.getMaxWindow(arr, 3);
         System.out.println(Arrays.toString(res));
     }
 
     private int[] getMaxWindow(int[] arr, int w) {
-        if(arr == null || w < 1 || arr.length < w){
+        if (arr == null || w < 1 || arr.length < w) {
             return null;
         }
 
@@ -20,15 +20,15 @@ public class _06MaxWindow {
         int[] res = new int[arr.length - w + 1];
 
         int index = 0;
-        for(int i = 0;i < arr.length;i++){
-            while(!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[i]){
+        for (int i = 0; i < arr.length; i++) {
+            while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[i]) {
                 qmax.pollLast();
             }
             qmax.addLast(i);
-            if(qmax.peekFirst() == i -w){
+            if (qmax.peekFirst() == i - w) {
                 qmax.pollFirst();
             }
-            if(i >= w - 1){
+            if (i >= w - 1) {
                 res[index++] = arr[qmax.peekFirst()];
             }
         }
--- src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/StackAndQueue/_07MonotonousStack.java
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/StackAndQueue/_07MonotonousStack.java
@@ -17,8 +17,8 @@ public class _07MonotonousStack {
         int[] arr = new int[]{3, 1, 3, 4, 3, 5, 3, 2, 2};
         int[][] res = monotonousStack.getNearLess(arr);
 
-        for(int i = 0;i<res.length;i++){
-            for(int j = 0;j < res[0].length;j++){
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res[0].length; j++) {
                 System.out.print(res[i][j] + " ");
             }
             System.out.println();
@@ -29,19 +29,19 @@ public class _07MonotonousStack {
         int[][] res = new int[arr.length][2];
         Stack<List<Integer>> stack = new Stack<>();
         for (int i = 0; i < arr.length; i++) {
-            while(!stack.empty() && arr[stack.peek().get(0)] > arr[i] ){
+            while (!stack.empty() && arr[stack.peek().get(0)] > arr[i]) {
                 List<Integer> indexes = stack.pop();
-                int leftIndex = stack.isEmpty()? -1: stack.peek().get(stack.peek().size() - 1);
+                int leftIndex = stack.isEmpty() ? -1 : stack.peek().get(stack.peek().size() - 1);
 
-                for (Integer index: indexes) {
+                for (Integer index : indexes) {
                     res[index][0] = leftIndex;
                     res[index][1] = i;
                 }
             }
 
-            if(!stack.isEmpty() && arr[stack.peek().get(0)] == arr[i]){
+            if (!stack.isEmpty() && arr[stack.peek().get(0)] == arr[i]) {
                 stack.peek().add(i);
-            }else{
+            } else {
                 List<Integer> list = new ArrayList<>();
                 list.add(i);
                 stack.push(list);
@@ -50,15 +50,15 @@ public class _07MonotonousStack {
 
         }
         //清算
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             List<Integer> indexes = stack.pop();
-            int leftIndex = stack.isEmpty()? -1: stack.peek().get(stack.peek().size() - 1);
-            for (Integer index: indexes) {
+            int leftIndex = stack.isEmpty() ? -1 : stack.peek().get(stack.peek().size() - 1);
+            for (Integer index : indexes) {
                 res[index][0] = leftIndex;
                 res[index][1] = -1;
             }
         }
 
-        return  res;
+        return res;
     }
 }

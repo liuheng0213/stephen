e589a8e67403e269b39e062d74e31f848c5a5878
Cooper <449836902@qq.com>
2020-01-10 01:44:40 +0800

    ff

--- src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/StackAndQueue/_08MaxSubMatrix.java
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/StackAndQueue/_08MaxSubMatrix.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class _08MaxSubMatrix {
     public static void main(String[] args) {
         _08MaxSubMatrix maxSubMatrix = new _08MaxSubMatrix();
-        int[][] map = new int[][]{{0, 0, 1, 1}, {0, 1, 0, 1}, {1, 1, 1, 0}};
+        int[][] map = new int[][]{{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
         int res = maxSubMatrix.maxRecSize(map);
         System.out.println(res);
     }

e0536663c8bef46adadb18becac57e57bcf7c0d5
Cooper <449836902@qq.com>
2020-01-31 17:28:46 +0800

    LIS

---
+++ src/main/java/basic/knowledge/stephen/algorithm/algorithm_4_Edition/ch3/BinarySearchDemo.java
@@ -0,0 +1,29 @@
+package basic.knowledge.stephen.algorithm.algorithm_4_Edition.ch3;
+
+public class BinarySearchDemo {
+    public static void main(String[] args) {
+        BinarySearchDemo binarySearchDemo = new BinarySearchDemo();
+        int[] arr = new int[]{2, 5, 7, 10, 12, 16, 19, 21, 25, 26};
+        int[] arr1 = new int[]{2};
+        int index = binarySearchDemo.search(arr1, 11);
+        System.out.println(index);
+    }
+
+    private int search(int[] arr, int target) {
+        return search(arr, 0, arr.length - 1, target);
+    }
+
+    private int search(int[] arr, int start, int end, int target) {
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid] > target){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
--- src/main/java/basic/knowledge/stephen/algorithm/leetcode/dynamic_programming/LongestIncreasingSubArr.java
+++ src/main/java/basic/knowledge/stephen/algorithm/leetcode/dynamic_programming/LongestIncreasingSubArr.java
@@ -1,46 +1,89 @@
 package basic.knowledge.stephen.algorithm.leetcode.dynamic_programming;
 
+/**
+ * 最长递增序列
+ */
 public class LongestIncreasingSubArr {
     public static void main(String[] args) {
-        int[] arr = new int[]{2,4,1,4,5,6,7};
-        getLongest(arr,6);
-        getLongestBetter(arr,6);
+        int[] arr = new int[]{2, 1, 5, 3, 6, 4, 8, 9, 7, 8, 9, 10, 13, 4, 6};
+        //int res = getLongest(arr, 1); //动态规划法
+        //System.out.println(res);
+        //int res2 = getLongestBetter(arr, 1);
+        //System.out.println(res2);
+
+        for (int i = 1; i <= arr.length; i++) {
+            System.out.println(getLongest(arr, i) == getLongestBetter(arr, i));
+        }
     }
 
     /**
-     * 动态规划
+     * 构建数组B 记录最小末尾
+     * B的索引记录子数组的长度,B的值记录该长度的在arr中的最小值
+     *
      * @param arr
-     * @param i
+     * @param n
+     * @return
      */
-    private static void getLongestBetter(int[] arr, int i) {
+    private static int getLongestBetter(int[] arr, int n) {
+        int[] B = new int[n];
+        if (B.length == 1) {
+            return 1;
+        }
+        int j = 1;
+        for (int i = 0; i < n; i++) {
+            int index = binarySearch(B, 0, j, arr[i]);
+            if (index > j) {
+                B[j] = arr[i];
+                j++;
+            } else {
+                B[index] = arr[i];
+            }
+        }
+        return j - 1;
     }
 
+    private static int binarySearch(int[] B, int start, int end, int target) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (B[mid] == target) {
+                return mid;
+            } else if (B[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+
     /**
+     * 动态规划法  d 数组 保留每一个的值
      * o(n^2)
+     *
      * @param arr
      * @param n
      */
-    public static void getLongest(int[] arr, int n){
+    public static int getLongest(int[] arr, int n) {
         //存放状态
         int[] d = new int[n];
         //最长非降子序列长度初始化为1
         int length = 1;
         //状态初始化为1
         d[0] = 1;
-        for (int i = 1; i < n; i++){
-            //状态初始化为1
-            d[i] = 1;
-            for (int j = 0; j < i; j++){
-                if (arr[i] > arr[j] && d[j] + 1 > d[i]){
+        for (int i = 1; i < n; i++) {
+            //状态初始化为1  zz
+            d[i] = 1;//自己算一个 G D
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && d[j] + 1 > d[i]) {
                     d[i] = d[j] + 1;
                 }
-                if (length < d[i]){
+                if (length < d[i]) {
                     length = d[i];
                 }
             }
         }
-
-        System.out.println(length);
+        return length;
 
     }
 

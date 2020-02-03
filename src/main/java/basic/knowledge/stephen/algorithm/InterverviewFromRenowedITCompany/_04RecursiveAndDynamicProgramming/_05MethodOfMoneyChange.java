a505aca6e7282e6f786a67b64f8c38cd8ca28e92
Cooper <449836902@qq.com>
2020-02-03 23:21:26 +0800

    _05MethodOfMoneyChange_DP

--- src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_04RecursiveAndDynamicProgramming/_05MethodOfMoneyChange.java
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_04RecursiveAndDynamicProgramming/_05MethodOfMoneyChange.java
@@ -3,8 +3,8 @@ package basic.knowledge.stephen.algorithm.InterverviewFromRenowedITCompany._04Re
 public class _05MethodOfMoneyChange {
     public static void main(String[] args) {
         _05MethodOfMoneyChange methodOfMoneyChange = new _05MethodOfMoneyChange();
-        int[] arr = new int[]{5, 10, 25, 1};
-        int methods = methodOfMoneyChange.getRes(arr, 15);
+        int[] arr = new int[]{5, 10, 25, 1,100,50};
+        int methods = methodOfMoneyChange.getRes(arr, 45);
         System.out.println(methods);
     }
 
---
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_04RecursiveAndDynamicProgramming/_05MethodOfMoneyChange_DP.java
@@ -0,0 +1,87 @@
+package basic.knowledge.stephen.algorithm.InterverviewFromRenowedITCompany._04RecursiveAndDynamicProgramming;
+
+public class _05MethodOfMoneyChange_DP {
+    public static void main(String[] args) {
+        _05MethodOfMoneyChange_DP methodOfMoneyChange_dp = new _05MethodOfMoneyChange_DP();
+        int[] arr = new int[]{5, 10, 25, 1};
+        int methods = methodOfMoneyChange_dp.getRes1(arr, 0);
+        System.out.println(methods);
+    }
+
+    /**
+     * o(n * aim)
+     *
+     * @param arr
+     * @param aim
+     * @return
+     */
+    private int getRes1(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        int n = arr.length;
+        int[][] dp = new int[n + 1][aim + 1];
+        for (int i = 0; i <= aim; i++) {
+            if (i == 0) {
+                dp[n][i] = 1;
+            } else {
+                dp[n][i] = 0;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+
+        //k 从大到小  aim 从小到大
+        for (int index = n - 1; index >= 0; index--) {
+            for (int rest = 1; rest <= aim; rest++) {
+//                int num = 0;
+//                for(int k = 0;k * arr[index] <= rest;k++){
+//                    num += dp[index + 1][rest - k * arr[index]];
+//                }
+                dp[index][rest] = dp[index + 1][rest];
+                dp[index][rest] += rest - arr[index] >= 0 ? dp[index][rest - arr[index]] : 0;
+            }
+        }
+        return dp[0][aim];
+    }
+
+    /**
+     * o (n * aim^2)
+     *
+     * @param arr
+     * @param aim
+     * @return
+     */
+    private int getRes(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        int n = arr.length;
+        int[][] dp = new int[n + 1][aim + 1];
+        for (int i = 0; i <= aim; i++) {
+            if (i == 0) {
+                dp[n][i] = 1;
+            } else {
+                dp[n][i] = 0;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+
+        //k 从大到小  aim 从小到大
+        for (int index = n - 1; index >= 0; index--) {
+            for (int rest = 1; rest <= aim; rest++) {
+                int num = 0;
+                for (int k = 0; k * arr[index] <= rest; k++) {
+                    num += dp[index + 1][rest - k * arr[index]];
+                }
+                dp[index][rest] = num;
+            }
+        }
+        return dp[0][aim];
+    }
+}

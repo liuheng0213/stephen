22371c23dd4ea591f295e94c6d02daa678fdead6
Cooper <449836902@qq.com>
2020-03-13 23:35:32 +0800

    binaryTree: _09GenerateBalBSTByOrderedArray

--- src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_03BinaryTree/_08EitherBSTOrCBT.java
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_03BinaryTree/_08EitherBSTOrCBT.java
@@ -1,5 +1,7 @@
 package basic.knowledge.stephen.algorithm.InterverviewFromRenowedITCompany._03BinaryTree;
 
+import java.util.LinkedList;
+
 /**
  * 判断一棵树 要么是否为搜索二叉树
  * 是否为完全二叉树
@@ -9,17 +11,17 @@ public class _08EitherBSTOrCBT {
         Node head = new Node(4);
 
         head.left = new Node(2);
-        head.left.left = new Node(1);
-        head.left.right = new Node(9);
+        //head.left.left = new Node(1);
+        //head.left.right = new Node(9);
 
         head.right = new Node(6);
-        head.right.left = new Node(5);
-        head.right.right = new Node(7);
+        //head.right.left = new Node(5);
+        //head.right.right = new Node(7);
         _08EitherBSTOrCBT bSTOrCBT = new _08EitherBSTOrCBT();
         //boolean isBST = bSTOrCBT.isBSTByRecursive(head);
-        boolean isBST = bSTOrCBT.isBST(head);
-        //boolean isCBT = bSTOrCBT.isCBT(head);
-        System.out.println(isBST);
+        //boolean isBST = bSTOrCBT.isBST(head);
+        boolean isCBT = bSTOrCBT.isCBT(head);
+        System.out.println(isCBT);
     }
 
     private boolean isBSTByRecursive(Node node) {
@@ -45,11 +47,47 @@ public class _08EitherBSTOrCBT {
         return new ReturnType(min, max, false);
     }
 
+    /**
+     * 检查是否完全二叉树
+     * 关键是要检查到所有的点
+     *
+     * 完全二叉树 有个特点
+     * 1  左节点没有填， 不能跳过它填右节点
+     * 2  不可能 右节点没填就将新的节点填在当前节点的子节点
+     * @param node
+     * @return
+     */
     private boolean isCBT(Node node) {
         if (node == null) {
-            return false;
+            return true;
         }
-        return false;
+        LinkedList<Node> queue = new LinkedList<>();
+        boolean hasRightBro = true;//当前节点有没有右兄弟节点
+        Node left = null;
+        Node right = null;
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            left = node.left;
+            right = node.right;
+            if ((!hasRightBro && (left != null || right != null)) || (left == null && right != null)) {
+                return false;
+            }
+
+            //如果进到这里：
+            //上面的if 是false
+
+            if (left != null) {
+                queue.add(left);
+            }
+            if (right != null) {
+                queue.add(right);
+            } else {
+                hasRightBro = false;
+            }
+        }
+
+        return true;
     }
 
     private boolean isBST(Node node) {
@@ -72,6 +110,8 @@ public class _08EitherBSTOrCBT {
                 if (mostRight.right == null) {
                     mostRight.right = cur;
                     cur = cur.left;
+                    //第一次到cur 是不需要判断preCur的
+                    //00Morris 遍历路线可知  这里是不能判断preCur的
                     continue;
                 } else {
                     mostRight.right = null;

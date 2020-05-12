0130f227344f0d135ad1779f32477fb7d2581e13
Cooper <449836902@qq.com>
2020-05-12 22:55:30 +0800

    LeetCode76

--- src/main/java/basic/knowledge/stephen/algorithm/leetcode/greedy/LeetCode76.java
+++ src/main/java/basic/knowledge/stephen/algorithm/leetcode/greedy/LeetCode76.java
@@ -1,4 +1,61 @@
 package basic.knowledge.stephen.algorithm.leetcode.greedy;
 
+import java.util.HashMap;
+
 public class LeetCode76 {
+    public static void main(String[] args) {
+        LeetCode76 leetCode76 = new LeetCode76();
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        String res = leetCode76.minWindow(s, t);
+        System.out.println(res);
+    }
+
+    public String minWindow(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!map.containsKey(ch)) {
+                map.put(ch, 1);
+            } else {
+                map.put(ch, map.get(ch) + 1);
+            }
+        }
+
+        int left = 0;
+        int right = 0;
+        int min = Integer.MAX_VALUE;
+        int count = t.length();
+        int leftIndex = 0;
+        int rightIndex = 0;
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+                if (map.get(ch) >= 0) {
+                    count--;
+                }
+            }
+
+            while (count == 0) {
+                if (right - left < min) {
+                    min = right - left;
+                    leftIndex = left;
+                    rightIndex = right;
+                }
+
+                char c = s.charAt(left);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) + 1);
+                    if (map.get(c) > 0) {
+                        count++;
+                    }
+                }
+                left++;
+            }
+            right++;
+        }
+
+        return min == Integer.MAX_VALUE ? "" : s.substring(leftIndex, rightIndex + 1);
+    }
 }
--- src/main/java/basic/knowledge/stephen/algorithm/leetcode/greedy/Leetcode53.java
+++ src/main/java/basic/knowledge/stephen/algorithm/leetcode/greedy/Leetcode53.java
@@ -1,4 +1,22 @@
 package basic.knowledge.stephen.algorithm.leetcode.greedy;
 
 public class Leetcode53 {
+    /**
+     * 如果之前的和小于0 则从新开始计算
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        if(nums ==null || nums.length ==0){
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        int cur = 0;
+        for(int i = 0;i< nums.length;i++){
+            cur += nums[i];
+            max = Math.max(max,cur);
+            cur = cur < 0 ? 0: cur;
+        }
+        return max;
+    }
 }

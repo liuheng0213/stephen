e75219138a5e80ad4f77a49931c0611e72533436
Cooper <449836902@qq.com>
2020-05-14 07:27:57 +0800

    _14CandyDistribution  need to be modified

--- src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_09others/_14CandyDistribution.java
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_09others/_14CandyDistribution.java
@@ -7,33 +7,34 @@ public class _14CandyDistribution {
         int[] arr = new int[]{9, 5, 4, 1, 6, 7, 8, 6, 5, 4, 3, 2, 1, 4, 12, 5};//answer  is 49
         int[] arr1 = new int[]{1, 5, 7, 9, 9, 6, 3, 4, 8, 10};
         int[] arr2 = new int[]{1, 2, 2};
-        int nums2 = candyDistribution2.getRes(arr1);
+        int[] arr3 = new int[]{1, 3, 2,2,1};
+        int nums2 = candyDistribution2.candy(arr3);
         System.out.println(nums2);
     }
 
 
-    private int getRes(int[] arr) {
-        if (arr == null || arr.length == 0) {
+    private int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) {
             return 0;
         }
         //初始得 res 不涉及到大小怎样加减得问题
-        int index = nextMaxIndex(arr, 0);
-        int res = getCandyNums(arr, 0, index++);
+        int index = nextMaxIndex(ratings, 0);
+        int res = getCandyNums(ratings, 0, index++);
         int upSlopes = 1;//1,2,3,4,5
         int downSlopes = 0;//5,4,3,2,1
         int rightCandyNums = 0;
         //刚入 while 必定是arr[index] < arr[index - 1]
-        while (index < arr.length) {
-            if (arr[index] < arr[index - 1]) {
-                int newIndex = nextMinIndex(arr, index - 1);
-                rightCandyNums = getCandyNums(arr, index - 1, newIndex++);
+        while (index < ratings.length) {
+            if (ratings[index] < ratings[index - 1]) {
+                int newIndex = nextMinIndex(ratings, index - 1);
+                rightCandyNums = getCandyNums(ratings, index - 1, newIndex++);
                 downSlopes = newIndex - index + 1;
                 res += rightCandyNums + (upSlopes > downSlopes ? -downSlopes : -upSlopes);
                 upSlopes = 1;
                 index = newIndex;
-            } else if (arr[index] > arr[index - 1]) {
-                int newIndex = nextMaxIndex(arr, index - 1);
-                rightCandyNums = getCandyNums(arr, index - 1, newIndex++);
+            } else if (ratings[index] > ratings[index - 1]) {
+                int newIndex = nextMaxIndex(ratings, index - 1);
+                rightCandyNums = getCandyNums(ratings, index - 1, newIndex++);
                 upSlopes = newIndex - index + 1;
                 res += rightCandyNums - 1;
                 downSlopes = 1;

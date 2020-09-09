7c1890c044d6c78f33020772835dd1cd91a86a36
Cooper <449836902@qq.com>
2020-09-09 18:27:26 +0800

    _02DeleteBackwardsToK

--- src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_02linkedList/_02DeleteBackwardsToK.java
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_02linkedList/_02DeleteBackwardsToK.java
@@ -1,8 +1,33 @@
 package basic.knowledge.stephen.algorithm.InterverviewFromRenowedITCompany._02linkedList;
 
-import java.util.Iterator;
-
+//在单链表中删除倒数第K个节点
 public class _02DeleteBackwardsToK {
+    public Node removeLastKthNode(Node node, int lastKth) {
+        if (node == null || lastKth < 1) {
+            return node;
+        }
+        Node cur = node;
+        while (cur != null) {
+            cur = cur.next;
+            lastKth--;
+        }
+        if (lastKth > 0) {
+            node = node.next;
+            return node;
+        }
+
+        if (lastKth <= 0) {
+            cur = node;
+            while (++lastKth != 0) {
+                cur = cur.next;
+            }
+
+            cur.next = cur.next.next;
+        }
+
+        return node;
+
+    }
 
 
 }

9ac17505a51dd196cc32b041b301e570a2463912
Cooper <449836902@qq.com>
2020-10-19 10:50:59 +0800

    _10LongestCommonSubSequence_DP

--- src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_04recursiveAndDynamicProgramming/_09LongsetCommonSubSet_DP.java
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_04recursiveAndDynamicProgramming/_09LongsetCommonSubSet_DP.java
@@ -2,6 +2,7 @@ package basic.knowledge.stephen.algorithm.InterverviewFromRenowedITCompany._04re
 
 import org.apache.commons.lang3.StringUtils;
 
+//求两个字符串共同的序列
 public class _09LongsetCommonSubSet_DP {
     public static void main(String[] args) {
         _09LongsetCommonSubSet_DP longsetCommonSubSet = new _09LongsetCommonSubSet_DP();
@@ -16,74 +17,72 @@ public class _09LongsetCommonSubSet_DP {
         if (StringUtils.isEmpty(str1) || StringUtils.isEmpty(str2)) {
             return "";
         }
+
         char[] chars1 = str1.toCharArray();
         char[] chars2 = str2.toCharArray();
+
         int[][] dp = getDp(chars1, chars2);
+
         int m = chars1.length - 1;
         int n = chars2.length - 1;
-        char[] res = new char[dp[m][n]];
-        int index = res.length - 1;
-        while (index >= 0) {
-            if (n > 0 && dp[m][n] == dp[m][n - 1]) {
-                n--;
-            } else if (m > 0 && dp[m][n] == dp[m - 1][n]) {
-                m--;
-            } else {
+
+        int resLen = dp[m][n];
+        char[] res = new char[resLen];
+        int index = resLen - 1;
+        while (m > 0 || n > 0) { //或者index >= 0
+            if (chars1[m] == chars2[n]) {
                 res[index--] = chars1[m];
                 m--;
                 n--;
+            } else {
+                if (m > 0 && dp[m][n] == dp[m - 1][n]) {
+                    m--;
+                } else if (n > 0) {
+                    n--;
+                }
             }
         }
-        return String.valueOf(res);
+
+        //出来时 必有 m== 0 && n == 0
+
+        return new String(res);
 
     }
 
     /**
      * dp 含义:
+     * dp[i][j] 表示 0..i; 0..j 相同的序列的长度
+     *
      * @param chars1
      * @param chars2
      * @return
      */
     private int[][] getDp(char[] chars1, char[] chars2) {
-        int m = chars1.length;
-        int n = chars2.length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = chars1[0] == chars2[0] ? 1 : 0;
-        for (int i = 1; i < m; i++) {
-            if (chars1[i] == chars2[0]) {
-                dp[i][0] = 1;
-            } else {
-                dp[i][0] = dp[i - 1][0];
-            }
+        int[][] dp = new int[chars1.length][chars2.length];
+        int mark = -1;
+        for (int i = 0; i < chars1.length; i++) {
+            dp[i][0] = Math.max(chars1[i] == chars2[i] ? 1 : 0, dp[i - 1][0]);
         }
 
-
-        for (int i = 1; i < n; i++) {
+        mark = -1;
+        for (int i = 0; i < chars2.length; i++) {
             if (chars1[0] == chars2[i]) {
+                mark = i;
                 dp[0][i] = 1;
-            } else {
-                dp[0][i] = dp[0][i - 1];
+            } else if (i >= mark) {
+                dp[0][i] = 1;
             }
         }
-
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 1; i < chars1.length; i++) {
+            for (int j = 1; j < chars2.length; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 if (chars1[i] == chars2[j]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
                 }
-
-                // 与上面的逻辑一样滴?　　
-//                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-//                if (chars1[i] == chars2[j]) {
-//                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
-//                }
-
             }
         }
 
         return dp;
+
     }
 }
--- src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_04recursiveAndDynamicProgramming/_10LongestCommonSubSequence_DP.java
+++ src/main/java/basic/knowledge/stephen/algorithm/InterverviewFromRenowedITCompany/_04recursiveAndDynamicProgramming/_10LongestCommonSubSequence_DP.java
@@ -1,78 +1,69 @@
 package basic.knowledge.stephen.algorithm.InterverviewFromRenowedITCompany._04recursiveAndDynamicProgramming;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class _10LongestCommonSubSequence_DP {
     public static void main(String[] args) {
         _10LongestCommonSubSequence_DP longestCommonSubSequence = new _10LongestCommonSubSequence_DP();
-        String str1 = "1AB2345CD";
+        String str1 = "1AB2345ECD";
         String str2 = "12345EF";
         String res = longestCommonSubSequence.lcst(str1, str2);
         System.out.println(res);
     }
 
     private String lcst(String str1, String str2) {
-        if (StringUtils.isEmpty(str1) || StringUtils.isEmpty(str2)) {
+        if (str1 == null || str2 == null) {
             return "";
         }
         char[] chars1 = str1.toCharArray();
         char[] chars2 = str2.toCharArray();
-        int[][] dp = getDp(chars1, chars2);
-        int end = 0;
-        int max = 0;
-        for (int i = 0; i < chars1.length; i++) {
-            for (int j = 0; j < chars2.length; j++) {
-                if (dp[i][j] > max) {
-                    end = j;
-                    max = dp[i][j];
-                }
-            }
-        }
+        int[] res = getDp(chars1, chars2);
 
-        return str2.substring(end - max + 1, end + 1);
+        return str1.substring(res[1] - res[0] + 1,res[1] + 1);
     }
 
     /**
      * 合理的构造dp,指定dp的含义:
      * 本体dp[i][j] 表示的是chars1[0..i] 且公共子串的最后字母为chars1[i];
      * chars2[0..j] 且公共子串的最后字母为chars2[j]
-     * 的最大子串
+     * 的最大子串的长度
+     * <p>
+     * 最大值并不一定
+     * 是dp[m -1][n - 1];
      *
-     *  最大值并不一定
-     *  是dp[m -1][n - 1];
      * @param chars1
      * @param chars2
      * @return
      */
-    private int[][] getDp(char[] chars1, char[] chars2) {
+    private int[] getDp(char[] chars1, char[] chars2) {
+
         int m = chars1.length;
         int n = chars2.length;
-
         int[][] dp = new int[m][n];
 
         for (int i = 0; i < m; i++) {
-            if (chars1[i] == chars2[0]) {
-                dp[i][0] = 1;
-            } else {
-                dp[i][0] = 0;
-            }
+            dp[i][0] = chars1[i] == chars2[0] ? 1 : 0;
         }
 
-        for (int j = 0; j < n; j++) {
-            if (chars2[j] == chars1[0]) {
-                dp[0][j] = 1;
-            } else {
-                dp[0][j] = 0;
-            }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = chars1[0] == chars2[i] ? 1 : 0;
         }
 
+        int maxLen = Integer.MIN_VALUE;
+        int rowMaxLenIndex = 0;
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (chars1[i] == chars2[j]) {
+                if(chars1[i] == chars2[j]){
                     dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                if(dp[i][j] > maxLen){
+                    maxLen = dp[i][j];
+                    rowMaxLenIndex = i;
                 }
             }
         }
-        return dp;
+
+        int[] res= new int[2];
+        res[0] = maxLen;
+        res[1] = rowMaxLenIndex;
+        return res;
     }
 }
